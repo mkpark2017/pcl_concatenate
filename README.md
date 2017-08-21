b89e9c42deb3c7203878207d6aeecb4d9c2c6050
mkpark2017 <mk.park@unist.ac.kr>
2017-08-21 20:07:19 +0900

    pcl_concatenate

---
+++ README.md
@@ -0,0 +1,2 @@
+# mk.park
+# mkpark
---
+++ pcl_concatenate/pcl_concatenate/src/tf_broadcaster.cpp
@@ -0,0 +1,20 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "my_tf_broadcaster");
+  ros::NodeHandle node;
+
+  tf::TransformBroadcaster br;
+  tf::Transform transform;
+
+  ros::Rate rate(50.0);
+
+  while (node.ok()){
+    transform.setOrigin( tf::Vector3(0.0, 2.0, 0.0) );
+    transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/uav1/velodyne", "world"));
+    rate.sleep();
+  }
+  return 0;
+};
